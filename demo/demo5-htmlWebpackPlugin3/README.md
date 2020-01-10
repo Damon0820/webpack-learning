062125f015a0c10b0e3ca7fba354a7a23bf836ca
fdm <245211612@qq.com>
2020-01-10 10:51:17 +0800

    使用htmlWebpackPlugin的多页面配置

---
+++ demo/demo5-htmlWebpackPlugin3/README.md
@@ -0,0 +1,4 @@
+# webpack-learning
+webpack从简到深，一步步实战代码例子
+
+- htmlWebpackPlugin 多页面配置
---
+++ demo/demo5-htmlWebpackPlugin3/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+function resolvePath(dir) {
+	return path.join(__dirname, dir)
+}
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		// main: './src/main.js'
+		main: resolvePath('./src/main.js'),
+		main2: resolvePath('./src/main2.js')
+	},
+	output: {
+		path: resolvePath('./dist'),
+		filename: 'js/[name]-[chunkhash:7].bundle.js',
+		// publicPath: 'http:cnd.com/' // script, link等引入资源时，加上公共路径
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			title: '爸爸的html', // 自定义属性，可在模板被获取
+			template: resolvePath('./public/index.html'),
+			filename: 'main.html',
+			chunks: ['main'] // 指定入口
+			// excludeChunks: ['mai2'] // 排除的入口
+		}),
+		new htmlWebpackPlugin({
+			title: '爸爸的html',
+			template: resolvePath('./public/index.html'),
+			filename: 'main2.html',
+			chunks: ['main2']
+		})
+	]
+}
