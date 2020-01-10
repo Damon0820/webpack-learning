91d06e768dd1dddf8f83411939bebf3a1a2a4e03
fdm <245211612@qq.com>
2020-01-10 18:11:43 +0800

    url-loader init

---
+++ demo/demo7-urlLoader/README.md
@@ -0,0 +1,4 @@
+# webpack-learning
+webpack从简到深，一步步实战代码例子
+
+- less-loader的配置（sass-loader同理）
---
+++ demo/demo7-urlLoader/src/page/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+import './index.less'
+import css from './index.css'
+// import template from './index.html'
+
+const app = {
+	// css,
+	// template
+}
+
+// app = {
+// 	css: 'none',
+// 	template: 'none'
+// }
+
+export default app
