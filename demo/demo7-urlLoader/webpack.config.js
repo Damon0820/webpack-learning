0269c608bf85dc6fb09b94f83663af6cd5f42eaa
fdm <245211612@qq.com>
2020-01-13 14:58:19 +0800

    url-loader 完善

--- demo/demo7-urlLoader/webpack.config.js
+++ demo/demo7-urlLoader/webpack.config.js
@@ -40,13 +40,36 @@ module.exports = {
 					{
 						loader: 'url-loader',
 						options: {
-							limit: 8192,
-							name: '[name].[hash:5].[ext]'
+							limit: 50000,
+							name: 'img/[name].[hash:7].[ext]'
 						}
 					}
 				]
-				
-			}
+			},
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 50000,
+							name: 'media/[name].[hash:7].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 50000,
+							name: 'fonts/[name].[hash:7].[ext]'
+						}
+					}
+				]
+			},
 		]
 	},
 	plugins: [
